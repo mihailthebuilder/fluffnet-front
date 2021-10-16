d24ab8db90e06d4b84a8190686129668901166e4
Mihail Marian <mihailmarian12@gmail.com>
2021-10-16 03:18:00 +0100

    placeholder url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import axios from "axios";
 
 function App() {
 
+  const requestUrl = window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/'
+    : 'https://desolate-harbor-87386.herokuapp.com/';
+
   return (
     <div className="App">
       Removed
