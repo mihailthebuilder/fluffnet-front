06552cc1e27ee339dffa28fc3b9b6f2031f10b3d
Mihail Marian <mihailmarian12@gmail.com>
2021-10-17 07:20:55 +0100

    added connection to backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import axios from "axios";
 
 function App() {
-  const requestUrl =
-    window.location.hostname === "localhost"
-      ? "http://localhost:5000/"
-      : "https://desolate-harbor-87386.herokuapp.com/";
+  const [apiResponse, setApiResponse] = useState("");
 
-  return <div className="App">Removed</div>;
+  const requestUrl =
+    window.location.hostname === "localhost" ? "http://localhost:5000/" : "";
+
+  useEffect(() => {
+    axios.post(requestUrl).then((res) => {
+      setApiResponse(res.data);
+      console.log(res);
+    });
+  }, [requestUrl]);
+
+  return <div className="App">{apiResponse ? "Removed" : apiResponse}</div>;
 }
 
 export default App;
