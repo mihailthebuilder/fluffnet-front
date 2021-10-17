e59d575792355cb95c49609ef7110d230650fb87
Mihail Marian <mihailmarian12@gmail.com>
2021-10-17 07:09:19 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import './App.scss';
+import "./App.scss";
 import axios from "axios";
 
 function App() {
+  const requestUrl =
+    window.location.hostname === "localhost"
+      ? "http://localhost:5000/"
+      : "https://desolate-harbor-87386.herokuapp.com/";
 
-  const requestUrl = window.location.hostname === 'localhost'
-    ? 'http://localhost:5000/'
-    : 'https://desolate-harbor-87386.herokuapp.com/';
-
-  return (
-    <div className="App">
-      Removed
-    </div>
-  );
+  return <div className="App">Removed</div>;
 }
 
 export default App;
