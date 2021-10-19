5abee646c11ea52fe43b2078b4fc31355bfdbf90
Mihail Marian <mihailmarian12@gmail.com>
2021-10-19 15:36:24 +0100

    added text for faq

--- src/App.js
+++ src/App.js
@@ -85,6 +85,34 @@ function App() {
           </span>
         </h2>
       </section>
+
+      <section id="faq">
+        <h2>FAQ</h2>
+        <div className="question">
+          <h3>How does this work?</h3>
+          <p>
+            Check out the source code{" "}
+            <a href="https://github.com/mihailthebuilder/fluffnet">here</a>.
+          </p>
+        </div>
+        <div className="question">
+          <h3>Do you store the images I upload?</h3>
+          <p>Nope.</p>
+        </div>
+        <div className="question">
+          <h3>
+            How can I let you know about an issue/error with the website/model?
+          </h3>
+          <p>
+            If you're a dev, you can raise a ticket on the{" "}
+            <a href="https://github.com/mihailthebuilder/fluffnet">
+              GitHub repo
+            </a>
+            . If not, you can send me a message on{" "}
+            <a href="https://www.linkedin.com/in/mihailmarian/">LinkedIn</a>.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
