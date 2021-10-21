062efbc0943f56c8eaa3dcc3d5e4d37a9b358e81
Mihail Marian <mihailmarian12@gmail.com>
2021-10-21 09:52:06 +0100

    added firebase

---
+++ src/resources/fire.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCqHep8arVct7xlRCZpOY5T8dU44FpygM",
+  authDomain: "fluffnet-ai.firebaseapp.com",
+  projectId: "fluffnet-ai",
+  storageBucket: "fluffnet-ai.appspot.com",
+  messagingSenderId: "443265600217",
+  appId: "1:443265600217:web:a0522ad0c63fcd5c8accc8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
